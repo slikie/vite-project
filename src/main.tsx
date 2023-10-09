f9034ff2dd0c3753536af3cb4d9d08a786c2bd73
RT <13197246+slikie@users.noreply.github.com>
2023-10-09 16:52:01 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/1",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+//    <App />
+  </React.StrictMode>,
+)
