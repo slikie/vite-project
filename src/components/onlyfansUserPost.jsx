cccf324e4226c3165ccfc8c3cd89610193c4c867
RT <13197246+slikie@users.noreply.github.com>
2023-10-10 00:06:30 +0800

    make it look better

--- src/components/onlyfansUserPost.jsx
+++ src/components/onlyfansUserPost.jsx
@@ -1,13 +1,11 @@
-import { useState, useEffect, useRef } from "react";
-import { useParams, Route } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 import { FixedSizeList as List } from 'react-window';
-// import LocalTheme from "../../theme";
-import { Modal, Image } from "@nextui-org/react";
+import { Modal, Image, Divider, Card, CardHeader, CardBody, CardFooter, Avatar } from "@nextui-org/react";
 import { PhotoProvider, PhotoView, PhotoSlider } from 'react-photo-view';
 
 export default function Page() {
   const { user } = useParams();
-  // const { styles, theme, colorScheme } = LocalTheme();
   const [responseData, setResponseData] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [displayedPosts, setDisplayedPosts] = useState([]);
@@ -96,19 +94,36 @@ export default function Page() {
       {displayedPosts.length ? (
         displayedPosts.map(post => (
           <div key={post.id} style={{ margin: 20 }}>
-            <div>{post.date}</div>
-            <div>{post.text}</div>
-
-            <div style={{ flexDirection: 'row', alignItems: 'center' }}>
-              {post.attachments.length > 0 &&
-                post.attachments.map((attach) => {
-                  if (/(\.m4v|\.mkv|\.mp4)$/.test(attach.url)) {
-                    return <VideoPlaceholder url={attach.url} key={attach.url} />;
-                  } else {
-                    return <ImageAttachment url={attach.url} key={attach.url} />;
-                  }
-                })}
-            </div>
+            <Card className="max-w-[550px]">
+              <CardHeader className="justify-between">
+                <div className="flex gap-5">
+                  {/* <Avatar isBordered radius="full" size="md" src={item.avatar} /> */}
+                  <div className="flex flex-col gap-1 items-start justify-center">
+                    <h4 className="text-small font-semibold leading-none text-default-600">{user}</h4>
+                  </div>
+                </div>
+              </CardHeader>
+              <CardBody className="px-3 py-0 text-small text-default-400">
+                <span className="pt-2">{post.text}</span>
+                <div className="flex">
+                  {post.attachments.length > 0 &&
+                    post.attachments.map((attach) => {
+                      if (/(\.m4v|\.mkv|\.mp4)$/.test(attach.url)) {
+                        return <VideoPlaceholder url={attach.url} key={attach.url} />;
+                      } else {
+                        return <ImageAttachment url={attach.url} key={attach.url} />;
+                      }
+                    })}
+                </div>
+              </CardBody>
+              <CardFooter className="gap-3">
+                <div className="flex gap-1">
+                  <p className="text-default-400 text-small">Date</p>
+                  <p className="font-semibold text-default-400 text-small">{post.date}</p>
+                </div>
+              </CardFooter>
+            </Card>
+            <Divider className="my-4" />
           </div>
         ))
       ) : (
