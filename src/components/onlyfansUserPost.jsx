2d95193f712540046ca49e639f71abf516219778
RT <13197246+slikie@users.noreply.github.com>
2023-10-15 03:18:12 +0800

    cleanup and use react-infinite-scroller to lazy load

--- src/components/onlyfansUserPost.jsx
+++ src/components/onlyfansUserPost.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
-import { FixedSizeList as List } from 'react-window';
+import InfiniteScroll from 'react-infinite-scroller';
 import { Input, Modal, Image, Divider, Card, CardHeader, CardBody, CardFooter, Avatar, Link } from "@nextui-org/react";
 import { PhotoProvider, PhotoView, PhotoSlider } from 'react-photo-view';
 
@@ -10,11 +10,8 @@ export default function Page() {
   const [errorMessage, setErrorMessage] = useState("");
   const [displayedPosts, setDisplayedPosts] = useState([]);
   const [page, setPage] = useState(1);
-  const [isVideoModalOpen, setIsVideoModalOpen] = useState(false);
-  const [visible, setVisible] = useState(false);
   const [filterValue, setFilterValue] = useState("");
-  // Add state to track the video URL to show in modal
-  const [videoModalURL, setVideoModalURL] = useState(null);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
 
   const videoExtensions = ['mkv', 'avi', 'mp4', 'ogv', 'webm', 'rmvb', 'flv', 'wmv', 'mpeg', 'mpg', 'm4v', '3gp', 'mov', 'ts'];
   const audioExtensions = ['mp3', 'wav', 'wma', 'aac', 'flac', 'm4a', 'ogg'];
@@ -28,13 +25,11 @@ export default function Page() {
     </div>
   );
 
-
   const FilePlayer = ({ url }) => (
     <>
       <p> {url}</p>
     </>
   );
-
 
   const AudioPlayer = ({ url }) => (
     <>
@@ -48,6 +43,7 @@ export default function Page() {
     <>
       <Image
         isZoomed
+        loading="lazy"
         width={240}
         src={url}
         classnames="m-5"
@@ -72,40 +68,23 @@ export default function Page() {
         });
     };
     fetchUserData();
+    console.log('fetch hook 1')
   }, []);
 
   useEffect(() => {
     setDisplayedPosts(responseData.filter(post =>
       post.text.toLowerCase().includes(filterValue.toLowerCase())
     ).slice(0, page * 10));
+    console.log('fetch hook 2')
 
+    setHasMoreItems(displayedPosts.length < responseData
+      .filter(post => post.text.toLowerCase().includes(filterValue.toLowerCase())).length);
   }, [page, responseData, filterValue]);
 
   const loadMore = () => {
     setPage((prevPage) => prevPage + 1);
   };
 
-  //         {isVideoModalOpen && (
-  //             <Modal
-  //                 animationType="slide"
-  //                 transparent={false}
-  //                 visible={isVideoModalOpen}
-  //             >
-  //                 <Modal.Header>
-  //                     <Button title="Close" onPress={() => setIsVideoModalOpen(false)} />
-  //                 </Modal.Header>
-  //                 <Video
-  //                     source={{ uri: videoModalURL }}
-  //                     resizeMode={ResizeMode.CONTAIN}
-  //                     useNativeControls
-  //                     shouldPlay
-  //                     onClose={() => setIsVideoModalOpen(false)}
-  //                     style={{ height: 200 }}
-  //                 />
-  //             </Modal>
-  //         )}
-  //     </div>
-  // );
   return (
     <div>
       <Input
@@ -116,56 +95,59 @@ export default function Page() {
       <Link href={`https://a.2345781.xyz/of/fetch/${user}?format=m3u8&query=${filterValue}`}>
         <button >m3u8 play</button>
       </Link>
-      {displayedPosts.length ? (
-        displayedPosts.map(post => (
-          <div key={post.id} style={{ margin: 20 }}>
-            <Card className="w-screen max-w-screen-md">
-              <CardHeader className="justify-between">
-                <div className="flex gap-5">
-                  {/* <Avatar isBordered radius="full" size="md" src={item.avatar} /> */}
-                  <div className="flex flex-col gap-1 items-start justify-center">
-                    <h4 className="text-small font-semibold leading-none text-default-600">{user}</h4>
+      <InfiniteScroll
+        loadMore={loadMore}
+        hasMore={hasMoreItems}
+        initialLoad={false}
+      >
+        {displayedPosts.length ? (
+          displayedPosts.map(post => (
+            <div key={post.id} style={{ margin: 20 }}>
+              <Card className="w-screen max-w-screen-md">
+                <CardHeader className="justify-between">
+                  <div className="flex gap-5">
+                    {/* <Avatar isBordered radius="full" size="md" src={item.avatar} /> */}
+                    <div className="flex flex-col gap-1 items-start justify-center">
+                      <h4 className="text-small font-semibold leading-none text-default-600">{user}</h4>
+                    </div>
                   </div>
-                </div>
-              </CardHeader>
-              <CardBody className="px-3 py-0 text-small text-default-400">
-                <span className="pt-2">{post.text}</span>
-                <div className="grid grid-flow-row-dense grid-cols-3">
-                  {post.attachments.length > 0 &&
-                    post.attachments.map(attachment => {
-                      const extension = attachment.url.split('.').pop();
-                      if (videoExtensions.includes(extension)) {
-                        return <VideoPlayer url={attachment.url} key={attachment.url} />;
-                      } else if (audioExtensions.includes(extension)) {
-                        return <AudioPlayer url={attachment.url} key={attachment.url} />;
-                      } else if (photoExtensions.includes(extension)) {
-                        return <ImagePlayer url={attachment.url} key={attachment.url} />;
-                      } else {
-                        return <FilePlayer url={attachment.url} key={attachment.url} />;
-                      }
-                    })
-                  }
-                </div>
-              </CardBody>
-              <CardFooter className="gap-3">
-                <div className="flex gap-1">
-                  <p className="text-default-400 text-small">Date</p>
-                  <p className="font-semibold text-default-400 text-small">{post.date}</p>
-                </div>
-              </CardFooter>
-            </Card>
-            <Divider className="my-4" />
-          </div>
-        ))
-      ) : (
-        <div>Loading...</div>
-      )}
-
-      {displayedPosts.length < responseData
-        .filter(post => post.text.toLowerCase().includes(filterValue.toLowerCase()))
-        .length && (
-          <button onClick={loadMore}>Load more</button>
-        )
+                </CardHeader>
+                <CardBody className="px-3 py-0 text-small text-default-400">
+                  <span className="pt-2">{post.text}</span>
+                  <div className="grid grid-flow-row-dense grid-cols-3">
+                    {post.attachments.length > 0 &&
+                      post.attachments.map(attachment => {
+                        const extension = attachment.url.split('.').pop();
+                        if (videoExtensions.includes(extension)) {
+                          return <VideoPlayer url={attachment.url} key={attachment.url} />;
+                        } else if (audioExtensions.includes(extension)) {
+                          return <AudioPlayer url={attachment.url} key={attachment.url} />;
+                        } else if (photoExtensions.includes(extension)) {
+                          return <ImagePlayer url={attachment.url} key={attachment.url} />;
+                        } else {
+                          return <FilePlayer url={attachment.url} key={attachment.url} />;
+                        }
+                      })
+                    }
+                  </div>
+                </CardBody>
+                <CardFooter className="gap-3">
+                  <div className="flex gap-1">
+                    <p className="text-default-400 text-small">Date</p>
+                    <p className="font-semibold text-default-400 text-small">{post.date}</p>
+                  </div>
+                </CardFooter>
+              </Card>
+              <Divider className="my-4" />
+            </div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </InfiniteScroll>
+      {hasMoreItems && (
+        <button onClick={loadMore}>Load more</button>
+      )
       }
     </div>
   );
