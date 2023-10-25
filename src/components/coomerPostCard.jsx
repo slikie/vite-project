00b51d1382930accf9f1916e43febb13c1f32dae
RT <13197246+slikie@users.noreply.github.com>
2023-10-25 18:38:48 +0800

    fmt f

--- src/components/coomerPostCard.jsx
+++ src/components/coomerPostCard.jsx
@@ -1,48 +1,48 @@
 import CoomerMediaPlayer from "./coomerMediaPlayer";
-import {  Card, CardBody, CardHeader, CardFooter, Divider, User } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, CardFooter, Divider, User } from "@nextui-org/react";
 import { Link } from "react-router-dom";
 
 export function PostCard({ post }) {
 
     return (
         <>
-        <Card className="w-screen max-w-screen-md">
+            <Card className="w-screen max-w-screen-md">
 
-            <CardHeader className="justify-between">
-                <Link to={`/user/${post.user}`} size="sm" isExternal>
-                    <User
-                        name={post.user}
-                        description={`@${post.user}`}
-                        avatarProps={{ src: `https://img.coomer.su/icons/${post.service}/${post.user}` }}
-                    />
-                </Link>
-            </CardHeader>
+                <CardHeader className="justify-between">
+                    <Link to={`/user/${post.user}`} size="sm" isExternal>
+                        <User
+                            name={post.user}
+                            description={`@${post.user}`}
+                            avatarProps={{ src: `https://img.coomer.su/icons/${post.service}/${post.user}` }}
+                        />
+                    </Link>
+                </CardHeader>
 
-            <CardBody className="px-3 py-0 text-small text-default-400">
+                <CardBody className="px-3 py-0 text-small text-default-400">
 
-                <b>{post.content}</b>
-                <div className="grid grid-flow-row-dense grid-cols-3">
-                    {post.attachments.length > 0 &&
-                        post.attachments.map(attachment => {
-                            return <CoomerMediaPlayer url={attachment.path} user={post.user} />
-                        })
-                    }
-                </div>
+                    <b>{post.content}</b>
+                    <div className="grid grid-flow-row-dense grid-cols-3">
+                        {post.attachments.length > 0 &&
+                            post.attachments.map(attachment => {
+                                return <CoomerMediaPlayer url={attachment.path} user={post.user} />
+                            })
+                        }
+                    </div>
 
-            </CardBody>
+                </CardBody>
 
-            <CardFooter className="gap-3">
+                <CardFooter className="gap-3">
 
-                <div className="flex gap-1">
-                    <b className="text-default-400 text-small">
-                        {post.published}
-                    </b>
-                </div>
+                    <div className="flex gap-1">
+                        <b className="text-default-400 text-small">
+                            {post.published}
+                        </b>
+                    </div>
 
-            </CardFooter>
+                </CardFooter>
 
-        </Card>
-                        <Divider className="my-4" />
-                        </>
+            </Card>
+            <Divider className="my-4" />
+        </>
     )
 }
