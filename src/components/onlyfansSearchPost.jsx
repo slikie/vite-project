4d5d7f2214654252c67dc72341a48398a21fd999
RT <13197246+slikie@users.noreply.github.com>
2023-10-25 17:06:41 +0800

    paging of the search

--- src/components/onlyfansSearchPost.jsx
+++ src/components/onlyfansSearchPost.jsx
@@ -1,41 +1,45 @@
 import { useState, useEffect } from "react";
 import { Input, Button, Card, CardBody, CardHeader, CardFooter, Avatar, Divider, User, Image } from "@nextui-org/react";
+import InfiniteScroll from 'react-infinite-scroller';
 import { Link, useNavigate } from "react-router-dom";
 
 export default function OFSearchPostComponent() {
 
     const [searchQuery, setSearchQuery] = useState("");
     const [posts, setPosts] = useState([]);
+    const [hasMore, setHasMore] = useState(true);
     const [page, setPage] = useState(0);
     const [errorMessage, setErrorMessage] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
-    const handleSearch = async () => {
-        setErrorMessage("");
-        setPosts([]);
-        setPage(0);
-        setIsLoading(true);
 
+    const loadMore = async () => {
         try {
-            const response = await fetch(`https://a.2345781.xyz/ofv2/posts?q=${searchQuery}&o=${page}`);
+            setErrorMessage("");
+            if (searchQuery === "") {
+                // if empty, set hasMore to false to stop loading more data
+                setHasMore(false);
+                return;
+            }
+            const url = `https://a.2345781.xyz/ofv2/posts?q=${searchQuery}&o=${page}`;
+            const response = await fetch(url);
             const data = await response.json();
             if (response.status === 404) {
                 setErrorMessage("404? maybe the api has been broken.");
                 return;
             }
             if (data.length === 0) {
-                setErrorMessage("No results found.");
+                setHasMore(false);
+            } else {
+                setHasMore(true);
+                setPosts(posts.concat(data));
+                setPage(page + 50);
             }
-            setPosts(prevPosts => [...prevPosts, ...data]);
-            // setPage(prevPage => prevPage + 50);
-            setIsLoading(false);
-
         } catch (error) {
             console.error(error);
             setErrorMessage("An unknown error occurred. Please try again later.");
-            setIsLoading(false);
         }
-    }
+    };
 
     return (
         <>
@@ -48,22 +52,25 @@ export default function OFSearchPostComponent() {
             <Button
                 icon="search"
                 loading={isLoading}
-                onClick={handleSearch}
+                onClick={loadMore}
             >
                 Search
             </Button>
-            {errorMessage ? (<h4> {errorMessage}</h4 >) : (posts.map(post => (
-                <PostCard key={post.id} post={post} />
-            )))}
+            {errorMessage ? (<h4> {errorMessage}</h4 >) : (
 
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={loadMore}
+                    hasMore={hasMore}
+                    loader={<div key={0}>Loading ...</div>}
+                >
+                    {posts.map((post) => (
+                        <PostCard key={post.id} post={post} />
+                    ))}
+                </InfiniteScroll>
 
-
-            {/* <Button
-                disabled={isLoading}
-                onClick={handleSearch}
-            >
-                Load more
-            </Button> */}
+            )}
+            <div>debug: post length {posts.length}</div>
         </>
     )
 }
